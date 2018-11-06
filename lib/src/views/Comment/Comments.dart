ac8aada39a849ef27016303d215a3a0a45e584a2
BdonGordon <bgordon@linamar.com>
2018-11-06 16:49:31 -0500

    - Hardcoded listview of what will soon be dynamically inputted comments
    TODO:
    - Redux creation of the list

--- lib/src/views/Comment/Comments.dart
+++ lib/src/views/Comment/Comments.dart
@@ -10,21 +10,36 @@ class Comments extends StatefulWidget {
 }
 
 class CommentsState extends State<Comments> {
+
   @override
   Widget build(BuildContext context) {
+    List<IComment> comments = <IComment>[];
+    IComment commentOne = new IComment(userName: "Jordy", commentText: "It's home time");
+    IComment commentTwo = new IComment(userName: "Brad", commentText: "I'm hungry");
+    comments.add(commentOne);
+    comments.add(commentTwo);
+
     // TODO: implement build
     return StoreConnector<AppState, IComment> (
       converter: (store) => store.state.commentState,
       builder: (context, comment) {
-        return ListView(
-            children: <Widget>[
-              ListTile(
-                title: Text(comment.userName),
-                subtitle: Text(comment.commentText),
-              )
-            ]
+        return Container(
+          child: buildComments(comments),
         );
       }
     );
+  }
+
+  Widget buildComments(List<IComment> comments) {
+    final Iterable<ListTile> commentTiles = comments.map((IComment comment){
+      return ListTile(
+        title: Text(comment.userName),
+        subtitle: Text(comment.commentText),
+      );
+    });
+
+    final List<Widget> commentWidgets = ListTile.divideTiles(tiles: commentTiles, context: context).toList();
+
+    return ListView(children: commentWidgets);
   }
 }
